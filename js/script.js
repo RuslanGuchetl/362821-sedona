1793e35c606e22702ca8c4528f3e2b3f64ac49ae
Ruslan <ruslanguchetl@mail.ru>
2017-05-11 22:31:48 +0300

    Финальные поправки

--- js/script.js
+++ js/script.js
@@ -2,11 +2,10 @@ var popup = document.querySelector(".modal-content");
 var search = popup.querySelector(".find-btn");
 var form = popup.querySelector(".form"); 
 var open = document.querySelector(".search-hotel-btn"); 
-
-  var arrive = popup.querySelector("[name=date-in]");
-  var leave = popup.querySelector("[name=date-out]");
-  var adult = popup.querySelector("[name=adult]");
-  var children = popup.querySelector("[name=child]");
+var arrive = popup.querySelector("[name=date-in]");
+var leave = popup.querySelector("[name=date-out]");
+var adult = popup.querySelector("[name=adult]");
+var children = popup.querySelector("[name=child]");
 
 open.addEventListener("click", function(event) { 
     event.preventDefault(); 
@@ -16,8 +15,8 @@ open.addEventListener("click", function(event) {
 search.addEventListener("click", function(event) { 
     if (!(arrive.value && leave.value && adult.value && child.value)) 
     { 
-        event.preventDefault();
-        popup.classList.add("modal-error"); 
+    event.preventDefault();
+    popup.classList.add("modal-error"); 
     }
 }); 
 
@@ -26,7 +25,7 @@ window.addEventListener("keydown", function(event)
     if (event.keyCode == 27) { 
     if (popup.classList.contains("modal-content-show")) 
       { 
-          popup.classList.remove("modal-content-show"); 
+      popup.classList.remove("modal-content-show"); 
       } 
     } 
 });
